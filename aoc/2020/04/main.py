c428496b38321dfc62c2eabd9425a324d14248a6
James Stidard <james@stidard.com>
2020-12-12 12:27:34 +0000

    day 7

--- aoc/2020/04/main.py
+++ aoc/2020/04/main.py
@@ -1,8 +1,31 @@
 import os
+from os.path import split
 
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 INPUT_FP = os.path.join(HERE, "input.txt")
 
+GROUP_DELIMITER = "\n\n"
+LINE_DELIMITER = "\n"
+LINE_ITEM_DELIMITER = " "
+
 with open(INPUT_FP, "r") as fp:
-    INPUT_DATA = fp.read()
+    INPUT_DATA = fp.read().strip()
+
+GROUPS = INPUT_DATA.split(GROUP_DELIMITER)
+ITEMS = [
+    [itm for lns in group.split(LINE_DELIMITER) for itm in lns.split(LINE_ITEM_DELIMITER)]
+    for group in GROUPS
+]
+
+print(ITEMS)
+
+group_items = []
+
+for group in INPUT_DATA.split(GROUP_DELIMITER):
+    group = group.replace(LINE_DELIMITER, LINE_ITEM_DELIMITER)
+    items = group.split(LINE_ITEM_DELIMITER)
+
+    group_items.append(items)
+
+print(group_items)
---
+++ aoc/2020/07/main.py
@@ -0,0 +1,77 @@
+import os
+import re
+
+
+SHINY_GOLD = "shiny gold"
+CONTAINER_BAG_RE = re.compile(
+    r"^(?P<name>[a-z ]+) bags contain (?P<inner_bags>.+)$", re.MULTILINE
+)
+INNER_BAGS_RE = re.compile(r"(?P<count>\d+) (?P<name>[a-z ]+) bags?(,|\.)")
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+INPUT_FP = os.path.join(HERE, "input.txt")
+
+
+with open(INPUT_FP, "r") as fp:
+    INPUT_DATA = fp.read()
+
+
+contents = {}
+
+for container in CONTAINER_BAG_RE.finditer(INPUT_DATA):
+    outer_name = container.group("name")
+    inner_bags = container.group("inner_bags")
+    contents[outer_name] = {}
+
+    for bag in INNER_BAGS_RE.finditer(inner_bags):
+        name = bag.group("name")
+        count = bag.group("count")
+        contents[outer_name][name] = int(count)
+
+
+def unpack(bag):
+    children = contents.get(bag, {})
+
+    for child, count in children.items():
+        yield child, count
+        yield from unpack(child)
+
+
+def take_until(predicate, iterable):
+    for item in iterable:
+        yield item
+
+        if predicate(item):
+            return
+
+
+def is_gold(bag):
+    return bag == SHINY_GOLD
+
+
+print("Part One")
+all_bags = list(contents.keys())
+contains_gold = []
+
+for bag in all_bags:
+    inner_bags = (name for name, _ in unpack(bag))
+    inner_bags = take_until(is_gold, inner_bags)
+    if SHINY_GOLD in set(inner_bags):
+        contains_gold.append(bag)
+
+print("count:", len(contains_gold))
+
+
+print("Part Two")
+
+
+def unpack_2(bag):
+    children = contents.get(bag, {})
+
+    for child, count in children.items():
+        for _ in range(count):
+            yield child
+            yield from unpack_2(child)
+
+
+print("count:", len(list(unpack_2(SHINY_GOLD))))
